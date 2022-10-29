ed3d082311637ebae1d75f62eb53bc6494a6598e
ksgwr <ksgwarai@gmail.com>
2022-10-29 16:29:25 +0900

    dialog test

--- src/views/RoomResults.vue
+++ src/views/RoomResults.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="dialog">
-    <div class="dialog__inner">
-      <h3>My Dialog</h3>
-      <p>Content</p>
-      <a class="btn" @click="close">Close</a>
-    </div>
-  </div>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+    >
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="close()"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: 'RoomResults',
+  data () {
+      return {
+          dialog: true,
+      }
+  },
   methods: {
     close () {
       this.$router.back()
